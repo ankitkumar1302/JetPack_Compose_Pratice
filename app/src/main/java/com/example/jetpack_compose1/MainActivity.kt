76c155f35e0391554e88d47ebe85179016a3e690
ankit <you@example.com>
2023-02-16 18:20:10 +0530

    Image from Internet

--- app/src/main/java/com/example/jetpack_compose1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose1/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import android.text.Selection
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -30,6 +31,7 @@ import androidx.compose.ui.text.font.createFontFamilyResolver
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import com.example.jetpack_compose1.ui.theme.MyFontFamily
 
 class MainActivity : ComponentActivity() {
@@ -49,9 +51,9 @@ class MainActivity : ComponentActivity() {
 //            LazyRowExample()
 //            LazyRowColumn()
 //            boxExample()
-            MaterialUiApp()
-
-
+//            MaterialUiApp()
+//            ImageAssetExample()
+            ImageInternetExample()
         }
     }
 }
@@ -239,11 +241,39 @@ fun boxExample() {
             )
         }
         Text(text = "This is box", color = Color.Red, modifier = Modifier.align(Alignment.Center))
+
     }
 }
+
 @Composable
-fun MaterialUiApp(){
+fun MaterialUiApp() {
+}
 
+@Composable
+fun ImageAssetExample() {
 
+    Image(
+        painter = painterResource(id = R.drawable.anime),
+        contentDescription = "About",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .border(5.5.dp, Color.Red, CircleShape)
+            .size(300.dp)
+            .clip(shape = CircleShape)
+    )
+}
+@Composable
+fun ImageInternetExample() {
 
+    Image(
+        painter = rememberImagePainter(
+            data = "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
+        ),
+        contentDescription = "About",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .border(5.5.dp, Color.Red, CircleShape)
+            .size(300.dp)
+            .clip(shape = CircleShape)
+    )
 }
